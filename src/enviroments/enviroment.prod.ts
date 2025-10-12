38bc2e8d3e3da2b25357baab6d5e7b0d3f070fce
AbisurDiazR <abisurdiazramirez@gmail.com>
2025-10-12 02:19:02 -0600

    Firebase deplou

---
+++ src/enviroments/enviroment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyClihmdsqD6MsgjH_fwXyV3zcQqrMI7-sA',
+    authDomain: 'consulta-ya.firebaseapp.com',
+    projectId: 'consulta-ya',
+    storageBucket: 'consulta-ya.firebasestorage.app',
+    messagingSenderId: '822746230389',
+    appId: '1:822746230389:web:36e7448063a0a2c367923c',
+    measurementId: 'G-NSSGM23R74',
+  },
+};
